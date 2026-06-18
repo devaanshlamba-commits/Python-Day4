9b7a4b8ec35cd257cfc71d5dc2d8fb59caf12025
devaanshlamba-commits <devaansh.lamba@gmail.com>
2026-06-18 20:58:33 +0530

    Devaansh

---
+++ Activity3.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number :"))
+if (num<15):
+    print("num is smaller than 15")
+    print("im in if block")
+else:
+    print("num is greater than 15")
+    print("im in else block")
+print("im nor in if neither in else")
